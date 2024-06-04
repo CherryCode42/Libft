d8cebf0c47c9f8768d4b6b69e0e81c84bc5a3cc0
IreHome <arefievairina@gmail.com>
2024-06-04 22:21:22 +0200

    04.06 Libft

---
+++ Libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iarefeva <iarefeva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 15:28:57 by iarefeva          #+#    #+#             */
+/*   Updated: 2024/05/31 20:54:26 by iarefeva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+	t_list	*node;
+
+	if (!lst)
+		return (NULL);
+	node = lst;
+	while (node->next)
+		node = node->next;
+	return (node);
+}
